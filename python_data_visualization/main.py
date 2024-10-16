396bf2c3a9d006afe43bd82856ed8d4e18666bf2
Kasia Jaromirska <kasia.jaromirska@gmail.com>
2024-10-16 15:41:25 +0200

    Reading csv with pandas and data for boxplot

--- python_data_visualization/main.py
+++ python_data_visualization/main.py
@@ -1,7 +1,9 @@
 import matplotlib.pyplot as plt
-import csv
+import pandas as pd
 from typing import List
 from numpy import arange
+from numpy import transpose
+import os
 
 # set the data location folder
 files_path = "./python_data_visualization/data"
@@ -11,21 +13,21 @@ file_names = ["rsel.csv", "cel-rs.csv", "2cel-rs.csv", "cel.csv", "2cel.csv"]
 legend_names = ["1-Evol-RS", "1-Coev-RS", "2-Coev-RS", "1-Coev", "2-Coev"]
 data_colors = ["blue", "green", "red", "black", "magenta"]
 
-def read_file(file_name: str) -> List[float]:
-    with open(f"{files_path}/{file_name}") as csvfile: 
-        data_reader = csv.reader(csvfile, delimiter = ',')
-        header = next(data_reader) # read the header line
+def read_file(file_name: str) -> tuple[List[float], List[float]]:
+    data_reader = pd.read_csv(f"{files_path}/{file_name}", skiprows=1, header=None)
+    
+    data = []
+    for _, row in data_reader.iloc[:-1].iterrows():
+        # read the average value for each of the runs
+        # multiply for better visualization
+        avg = row[2:].mean() * 100
+        data.append(avg)
+    
+    boxplot_data = data_reader.iloc[-1, 2:].tolist()
+    
+    return(data, boxplot_data)
 
-        data = []
-        for row in data_reader:
-            # read the average value for each of the runs
-            # multiply for better visualization
-            avg = sum(list(map(float, row[2:]))) / len(list(map(float, row[2:]))) * 100 
-            data.append(avg)
-
-        return data
-
-def draw_plot(combined_data: List[List[float]]) -> None:
+def draw_plot(combined_data: List[List[float]], boxplot_combined_data: List[List[float]]) -> None:
     fig, axs = plt.subplots(1, 2, figsize=(6, 6))
 
     # add data to the first subplot
@@ -33,7 +35,9 @@ def draw_plot(combined_data: List[List[float]]) -> None:
         axs[0].plot(data, label=legend_names[i], linewidth=0.8, color=data_colors[i])
 
     # add data to the second subplot
-    axs[1].plot(data)
+    # something doesnt work here
+    transposed_boxplot_data = transpose(boxplot_combined_data)
+    axs[1].boxplot(boxplot_combined_data, tick_labels=legend_names, notch=True)
 
     # apply common changes
     for ax in axs:
@@ -45,7 +49,7 @@ def draw_plot(combined_data: List[List[float]]) -> None:
     # changes to the first subplot
     #
     # TODO: configure the grid to match the result
-    axs[0].grid(which='both', linestyle='dotted', linewidth=0.8, color='gray') 
+    axs[0].grid(which='both', linestyle='dotted', linewidth=1.2, color='gray') 
     axs[0].set_title("Pokolenie")
     axs[0].set_ylabel("Odsetek wygranych gier [%]")
     axs[0].legend(loc="lower right")
@@ -62,6 +66,8 @@ def draw_plot(combined_data: List[List[float]]) -> None:
     # changes to the second subplot
     #
     axs[1].yaxis.tick_right()
+    axs[1].grid(which='both', linestyle='dotted', linewidth=1.2, color='gray') 
+    
 
     plt.show()
 
@@ -81,11 +87,15 @@ def main() -> None:
 
     # read the data
     combined_data = []
+    boxplot_combined_data = []
     for name in file_names:
-        combined_data.append(read_file(name))
+        (data, boxplot_data) = (read_file(name))
+        combined_data.append(data)
+        boxplot_combined_data.append(boxplot_data)
     
-    draw_plot(combined_data);
+    draw_plot(combined_data, boxplot_combined_data)
 
 
 if __name__ == "__main__":
+    pd.options.display.max_rows = 9999
     main()
