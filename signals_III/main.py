b8855308b3a39ae9ea69b0641c9ecc789a2bb0a0
Frostyl <lgrobelny66@gmail.com>
2024-12-30 23:33:19 +0100

    Add exemplary data to signals_3

---
+++ signals_III/main.py
@@ -0,0 +1,12 @@
+import numpy as np
+import sys
+
+def read_signal(filename):
+    pass
+
+def main(argv):
+    for arg in argv:
+        print(arg)
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
